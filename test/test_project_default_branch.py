e01ac1f9d30fac1cdd470a4ac17a12bf95685ba3
ivan-gomes <ivan-gomes@users.noreply.github.com>
2021-06-07 11:59:38 -0500

    ST5AS-135 feat: regenerate for 2021-05

--- test/test_project_default_branch.py
+++ test/test_project_default_branch.py
@@ -16,11 +16,11 @@ import unittest
 import datetime
 
 import sysml_v2_api_client
-from sysml_v2_api_client.models.project import Project  # noqa: E501
+from sysml_v2_api_client.models.project_default_branch import ProjectDefaultBranch  # noqa: E501
 from sysml_v2_api_client.rest import ApiException
 
-class TestProject(unittest.TestCase):
-    """Project unit test stubs"""
+class TestProjectDefaultBranch(unittest.TestCase):
+    """ProjectDefaultBranch unit test stubs"""
 
     def setUp(self):
         pass
@@ -29,26 +29,21 @@ class TestProject(unittest.TestCase):
         pass
 
     def make_instance(self, include_optional):
-        """Test Project
+        """Test ProjectDefaultBranch
             include_option is a boolean, when False only required
             params are included, when True both required and
             optional params are included """
-        # model = sysml_v2_api_client.models.project.Project()  # noqa: E501
+        # model = sysml_v2_api_client.models.project_default_branch.ProjectDefaultBranch()  # noqa: E501
         if include_optional :
-            return Project(
-                type = 'Project', 
-                default_branch = sysml_v2_api_client.models.project_default_branch.Project_defaultBranch(
-                    id = '0', ), 
-                description = '0', 
-                id = '0', 
-                name = '0'
+            return ProjectDefaultBranch(
+                id = '0'
             )
         else :
-            return Project(
+            return ProjectDefaultBranch(
         )
 
-    def testProject(self):
-        """Test Project"""
+    def testProjectDefaultBranch(self):
+        """Test ProjectDefaultBranch"""
         inst_req_only = self.make_instance(include_optional=False)
         inst_req_and_optional = self.make_instance(include_optional=True)
 
